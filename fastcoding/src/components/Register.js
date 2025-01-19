9ff329882a5001e663a6493b547d2a570353af68
fastcodingx <fastcodingx@gmail.com>
2025-01-19 22:26:21 +0530

    Update Register.js

--- fastcoding/src/components/Register.js
+++ fastcoding/src/components/Register.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useState } from "react";
+import "../styles/Login.css";
+import { FcGoogle } from "react-icons/fc";
+import { Link } from "react-router-dom";
+
 
 const Register = () => {
   return (
-    <div>Register</div>
-  )
-}
+    <div className="login-main-container">
+      <div className="login-content">
+        <div className="login-content-1">
+          <p>Please enter your details</p>
+          <div className="login-content-input-1">
+            <input name="email" placeholder="Enter Email ID" />
+          </div>
+          <div className="login-content-input-1">
+            <input
+              name="password"
+              type="password"
+              placeholder="Enter Password"
+            />
+          </div>
+          <div className="login-content-input-1">
+            <input
+              name="confirmPassword"
+              type="password"
+              placeholder="Confirm Password"
+            />
+          </div>
+          <button className="login-content-btn">
+            Register
+          </button>
 
-export default Register
+          <div className="login-footer-content">
+            <div className="login-content-google">
+              <p style={{fontSize:"20px"}}>Sign in with Google</p>
+              <FcGoogle style={{ height: "30px", width: "30px" }} />
+            </div>
+            <p style={{ marginTop: "20px",color:"var(--secondary)",fontSize:"20px" }}>
+              I have an account?{" "}
+              <Link to={"/login"} className="login-forgot">
+                Login
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
